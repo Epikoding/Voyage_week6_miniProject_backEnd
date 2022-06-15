de6d1c1f22ce6f79324c6538a02fb69c8e05e797
Lee Jeong Chan <biolkj28@gmail.com>
2022-06-16 02:08:05 +0900

    1. mulitibag 오류 수정 oneToMany set으로 변경

---
+++ src/main/java/com/tenzo/mini_project2/web/dto/commentDto/CommentsResponseDto.java
@@ -0,0 +1,31 @@
+package com.tenzo.mini_project2.web.dto.commentDto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import com.tenzo.mini_project2.security.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class CommentsResponseDto {
+    private Long id;
+    private String nickname;
+    private String content;
+    private LocalDateTime createdAt;
+
+    @QueryProjection
+    public CommentsResponseDto(
+            Long id,
+            User user,
+            String content,
+            LocalDateTime createdAt
+    ) {
+        this.id = id;
+        this.nickname = user.getNickname();
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+}
